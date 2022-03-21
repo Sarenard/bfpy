2e0bc28fadeb42e8d873a4eea21ed0a0323fe3a5
Loris <monestierloris11@gmail.com>
2022-03-21 20:18:38 +0100

    morpion fin

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# python .\main.py -f .\input.bfr -r
+
 # TODO : LISTS
 
 from src.generator import Generator
@@ -12,6 +14,7 @@ parser.add_argument('-d', '--debug', help='mode debug', required=False, action='
 parser.add_argument('-di', '--debuginterpreteur', help='mode debug', required=False, action='store_true')
 parser.add_argument('-rc', '--runc', help='mode debug', required=False, action='store_true')
 parser.add_argument('-rp', '--runp', help='mode debug', required=False, action='store_true')
+parser.add_argument('-r', '--run', help='mode debug', required=False, action='store_true')
 parser.add_argument('-stfu', '--stfu', help='mode debug', required=False, action='store_true')
 args = parser.parse_args()
 
@@ -31,11 +34,20 @@ with open("sortie.bf", "w") as f:
     f.write(instructions)
 
 if not args.stfu : print("[INFO] : Execution du code")
-t = time.time()
-if args.runc:
-    os.system(".\src\interpreteur.exe .\sortie.bf")
-    
-if args.runp:
-    from src.interpreteur import interpreter
-    interpreter(instructions, args.debuginterpreteur)
+if not args.run:
+    t = time.time()
+    if args.runc:
+        os.system(".\src\interpreteur.exe .\sortie.bf")
+        
+    if args.runp:
+        from src.interpreteur import interpreter
+        interpreter(instructions, args.debuginterpreteur)
+else:
+    t = time.time()
+    longueur = len(instructions)
+    if longueur > 10000:
+        os.system(".\src\interpreteur.exe .\sortie.bf")
+    else:
+        from src.interpreteur import interpreter
+        interpreter(instructions, args.debuginterpreteur)
 if not args.stfu : print(f"[INFO] : Execution du code terminée en {time.time() - t} secondes")
