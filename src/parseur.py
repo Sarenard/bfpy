1f3facac03c1b1b1fc63cceb9e93e24566507e8b
Loris <monestierloris11@gmail.com>
2022-03-30 14:50:51 +0200

    spaces in strings

--- src/parseur.py
+++ src/parseur.py
@@ -1,8 +1,31 @@
 class Parseur:
     def __init__(self, debug):
-        self.instructions = ""
+        self.instructions = []
+        self.temp_string = ""
+        self.string = False
     def parse(self, code):
         print(f"[DEBUG PARSEUR] : Parsing : {code}")
-        self.instructions = [x for x in ("\n".join([x for x in code.split("\n") if not x.strip().startswith("//")]).replace("\n", " ")+" main").split(" ") if x != ""]
+        code = code.replace("\n", " ")+" main"
+        for truc in code.split(" "):
+            adder = self.instructions
+            if truc.strip().startswith("//"):
+                continue
+            elif truc == "":
+                continue
+            elif truc.startswith("\"") and not truc.endswith("\""):
+                self.string = True
+                self.temp_string += truc
+            elif truc.endswith("\"") and not truc.startswith("\""):
+                self.string = False
+                self.temp_string += truc
+                self.instructions.append(self.temp_string.replace(" ", "\\N"))
+                self.temp_string = ""
+            elif truc.startswith("\"") and truc.endswith("\""):
+                self.instructions.append(truc.replace(" ", "\\N"))
+            elif self.string:
+                self.temp_string += f" {truc}"
+            else:
+                self.instructions.append(truc)
+            
         print(f"[DEBUG PARSEUR] : Parsing terminé {self.instructions}")
         return self.instructions
