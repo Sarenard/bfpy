766aee1c9ab3a59ad55d58b096043eecaf8dcd9a
Loris <monestierloris11@gmail.com>
2022-04-05 11:21:58 +0200

    += -= mandelbrot.bf

--- src/parseur.py
+++ src/parseur.py
@@ -7,12 +7,12 @@ class Parseur:
     def parse(self, code):
         if self.debug : print("[DEBUG PARSEUR] : Parsing : "+code.replace("\n", "\\n"))
         code = code.replace("\n", " ")+" main"
-        for truc in code.split(" "):
-            adder = self.instructions
-            if truc.strip().startswith("//"):
-                continue
-            elif truc == "":
-                continue
+        code = code.split(" ")
+        x = 0
+        while x < len(code):
+            truc = code[x]
+            if truc.strip().startswith("//") or truc == "":
+                pass
             elif truc.startswith("\"") and not truc.endswith("\""):
                 self.string = True
                 self.temp_string += truc
@@ -26,7 +26,16 @@ class Parseur:
             elif self.string:
                 self.temp_string += f" {truc}"
             else:
-                self.instructions.append(truc)
-            
+                if truc in ["-=", "+="]:
+                    variable, integer, x = self.instructions.pop(), int(code[x+1]), x+1
+                    if integer == 0:
+                        pass
+                    elif integer > 0:
+                        self.instructions += [f"-{integer}", variable, "cadd"]
+                    else:
+                        self.instructions += [f"{integer}", variable, "cadd"]
+                else:
+                    self.instructions.append(truc)
+            x += 1
         if self.debug : print(f"[DEBUG PARSEUR] : Parsing terminé {self.instructions}")
         return self.instructions
